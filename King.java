cad93b2e6140a2dccfe4e12e5a72db56e64219ad
deaamonder <132516450+deaamonder@users.noreply.github.com>
2024-02-01 13:29:33 +0200

    Add files via upload

--- King.java
+++ King.java
@@ -1,36 +1,45 @@
 import java.util.ArrayList;
 
-public class King extends ConcretePiece{
+public class King extends ConcretePiece {
     Player Owner;
     int Number;
     int Movekilled;
     Position Pos;
-    ArrayList<Position> Moveh= new ArrayList<>();
-    public King(Position pos,Player owner){
-        Pos=pos;
+    ArrayList<Position> Moveh = new ArrayList<>();
+
+    public King(Position pos, Player owner) {
+        Pos = pos;
         Owner = owner;
+        Moveh.add(pos);
     }
 
     @Override
-    public Player getOwner(){
+    public Player getOwner() {
         return this.Owner;
     }
+
     @Override
-    public String getType(){
+    public String getType() {
         return "♔";
     }
-    public void Moved(Position a){
+
+    public void Moved(Position a) {
         Moveh.add(a);
     }
-    public void Setpos(Position a){
-        this.Pos=a;
+
+    public void Setpos(Position a) {
+        this.Pos = a;
     }
-    public Position Getpos(){
+
+    public Position Getpos() {
         return Pos;
     }
-    public void setMovekilled(int a){
-        this.Movekilled=a;
+
+    public void setMovekilled(int a) {
+        this.Movekilled = a;
     }
-    public void setNumber(int n){this.Number=n;}
-    public int getNumber(){return this.Number;}
+
+    public void setNumber(int n) {
+        this.Number = n;
+    }
 }
